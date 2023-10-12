30d500d85bb6d5e6ba630dd01979490f5b36fcd9
sava9ecode <sava9emode@yandex.ru>
2023-10-12 14:48:13 +0300

    Add views, urls, templates

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.ListListView.as_view(), name="index"),
+    path("list/<int:list_id>/", views.ItemListView.as_view(), name="list"),
 ]
